9e9aaffb26ae660965662c2aeaf50d859daa061b
Dana <dana.stelling@xs4all.nl>
2026-09-17 19:35:21 +0200

    Created bag of words per dialog act

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,11 +1,19 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
+from collections import Counter
 
-#loading the .dat file into a dataframe and separate into training and test set
+#loading the .dat file and separate into training and test set
 with open('dailog_acts.dat') as file:
     dat= [row.strip().lower().split(maxsplit=1) for row in file]
 
-data= pd.DataFrame(dat, columns=["dailog_act","utterance"])
-training_data, test_data= train_test_split(data,test_size=0.15, random_state=42)
+training_data, test_data= train_test_split(dat,test_size=0.15, random_state=42)
 
-user_sentence= input().lower()
+#creating bag of words per dailog act with a counter for the number of words per bag
+bag_of_words_dict= dict()
+for items in training_data:
+    dialog_label= items[0]
+    dialog_utterance= items[1].split()
+    if dialog_label not in bag_of_words_dict:
+        bag_of_words_dict[dialog_label]=Counter()
+    bag_of_words_dict[dialog_label].update(dialog_utterance)
+print(bag_of_words_dict)
