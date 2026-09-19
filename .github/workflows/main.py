764fd85dc740914e2dbae10e7ecf0e07cb150aaa
Dana <dana.stelling@xs4all.nl>
2026-09-19 16:01:55 +0200

    rule based at 80 percent

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from collections import Counter
+import re
 
 #loading the .dat file and separate into training and test set
 with open('dailog_acts.dat') as file:
@@ -17,38 +18,46 @@ for items in training_data:
         bag_of_words_dict[dialog_label]=Counter()
     bag_of_words_dict[dialog_label].update(dialog_utterance)
 
-#rule based function to decide a dialog act per sentence
-def classification_dialog_sentence(sentence):
-    if "thank" in sentence or "thank you" in sentence:
-        return "thank you"
-    if "noise" in sentence or "unintelligible" in sentence or "sil" in sentence or "cough" in sentence:
-        return "null"
-    if "ye"in sentence or "right" in sentence or "correct" in sentence:
-        return "affirm"
-    if "start" in sentence or "reset" in sentence:
+
+
+nationalities=["spanish","catelan","italian","indian","lebanese", "swedish", "japanese", "mexican", "hungarian","russian", "greek", "irish",
+                "cuban", "australian","danish", "welsh", "swiss","singaporean", "polish", "indonesian"
+               "afghan", "malaysian", "english", "moroccan", "jamaican", "eritrean","scottish","romanian","german","austrian","brazilian"
+               "thai","chinese","vietnamese","korean","turkish","portugese","french","british","american","tailand",
+               "asian", "oriental","european", "mediterranean", "international", "african","caribbean","cantonese","polynesian",
+                "corsica", "basque", "scandinavian", "tuscan","persian","venetian","belgian","australian" ]
+
+#function to classify sentence with a dialog_act label
+def classification(sentence):
+    if re.search(r"\b(start again|reset|restart|start over)\b", sentence):
         return "restart"
-    if "wrong" in sentence or "dont want" in sentence or "not" in sentence:
+    if re.search(r"\b(wrong|dont want|not)\b", sentence):
         return "deny"
-    if "repeat" in sentence or "back" in sentence or "again" in sentence:
-        return "repeat" 
-    if "no" in sentence:
-        return "negate" 
-    if "hello" in sentence:
-        return "hello"
-    if "it" in sentence and ("does" in sentence or "is" in sentence):
-        return "confirm"
-    if "could" in sentence or "address" in sentence or "number" in sentence or "phone" in sentence or "what" in sentence or "code" in sentence:
-        return "request"
-    if ("how" in sentence and "about" in sentence) or ("anything" in sentence and "else" in sentence):
+    if re.search(r"\b(repeat|back|again)\b", sentence):
+        return "repeat"
+    if re.search(r"\b(how about|anything else|what about|next)\b", sentence):
         return "reqalts"
-    if "bye" in sentence or "goodbye" in sentence:
+    if re.search(r"\bno\b", sentence):
+        return "negate"
+    if re.search(r"\b(ye|yeah|yea|perfect|right|correct|yes)\b", sentence):
+            return "affirm"
+    if re.search(r"\b(hello|hi|halo)\b", sentence):
+        return "hello"
+    if re.search(r"\b(could|address|number|what|phone number|post code|postcode|may i have|whats the|can i have the|where|price range)\b",sentence):
+        return "request"
+    if re.search(r"\b(it does|it is|does it|is it|do they serve|is there a .* restaurant|is that .* food)\b", sentence):
+        return "confirm"
+    if re.search(r"\b(thank|thank you|thanks)\b",sentence):
+            return "thank you"
+    if re.search(r"\b(bye|goodbye|good bye)\b", sentence):
         return "bye"
-    elif "can" in sentence or "spanish" in sentence or "catalan" in sentence or "food" in sentence or "restaurant" in sentence or "town" in sentence or "spanish" in sentence:
+    if re.search(rf"\b({'|'.join(nationalities)}|can|food|eartrain|center|matter|moderate|dont care|restaurant|town|moderately|looking|north|west|centre|east|south|any|cheap|seafood|expensive|bistro)\b", sentence):
         return "inform"
-    elif "okay" in sentence or "kay" in sentence:
+    if re.search(r"\b(okay|kay)\b",sentence):
         return "ack"
-    elif "more" in sentence:
+    if re.search(r"\bmore\b", sentence):
         return "reqmore"
+    else: return "null"
 
 #function to test the accuracy of the assigned dialog act
 def test_accuracy(test_data):
@@ -56,10 +65,10 @@ def test_accuracy(test_data):
     total_count=0
     for items in test_data:
         total_count+=1
-        if classification_dialog_sentence(items[1])==items[0]:
+        if classification(items[1])==items[0]:
             correct_count+=1
     return correct_count/total_count
 
-print(test_accuracy(test_data))
+print(test_accuracy(test_accuracy))
 
 
